f41def34246600784987b8e70708adb3d048d1bf
Olga Provotorova <sputnik-9@yandex.ru>
2021-03-23 18:02:39 +0300

    конвертер с изменением качества картинок.

--- conv_for_many.py
+++ conv_for_many.py
@@ -7,18 +7,19 @@ from pdf2image.exceptions import (
     PDFPageCountError,
     PDFSyntaxError
 )
-#Для работы библиотеки pdf2image на windows нужно установить poppler - https://blog.alivate.com.au/poppler-windows/
+
+# Для работы библиотеки pdf2image на windows нужно установить poppler - https://blog.alivate.com.au/poppler-windows/
 
 
 # берём все pdf файлы из своей директории
 pdf_files = [filename for filename in os.listdir(
     '.') if filename.endswith('.pdf')]
-#прописываем путь сохранения.
-save_dir = 'C:/Users/sputnik/Арг/Семь сестёр'
+# прописываем путь сохранения.
+save_dir = '../Семь сестёр/1'
 
 for pdf_file in pdf_files:
-    images = convert_from_path(pdf_file)
+    images = convert_from_path(pdf_file, size=(None, 1000), transparent=True,
+                               jpegopt={"progressive": True})
 
     for i, image in enumerate(images):
         image.save(os.path.join(save_dir, pdf_file.replace('.pdf', '') + '_' + str(i + 1).zfill(3) + '.jpg'), 'JPEG')
-
